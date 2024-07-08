2ac3a38a125925d203a49d06aa6123d3c8ea893b
Rami-Ismael <rami.m.ismael@gmail.com>
2024-07-08 15:54:16 -0500

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { index } from "drizzle-orm/mysql-core";
 import { url } from "inspector";
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mock_urls: string[] = [
   "https://utfs.io/f/2e3a5f02-7fc1-497a-9ee5-d82167272c67-jp8mqc.png" , 
@@ -14,12 +15,22 @@ const mock_images = mock_urls.map( ( url , index ) => ( {
   url,
 } ) );
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log( posts );
+
   return (
     <main >
       <div className="flex flex-wrap gap-4">
-        { mock_images.map( ( image ) => (
-          <div key={image.id} className="w-48">
+        {
+          posts.map( ( post) => (
+            <div key = {post.id}> {post.name} </div>
+
+          ))
+        }
+        { mock_images.map( ( image , index ) => (
+          <div key={image.id + "-"+ index } className="w-48">
             <img src={image.url} />
           </div>
         ) ) }
