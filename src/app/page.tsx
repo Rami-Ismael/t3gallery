37fc321fab14e9d9d1cab4fdc0d7547838fcbddd
Rami Ismael <rami.m.ismael@gmail.com>
2025-07-18 16:43:36 -0500

    Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { id } from "zod/v4/locales";
+import {db} from "~/server/db";
+
 const mockUrls = [
   "https://z68hkk8q7p.ufs.sh/f/upbel1v8YSdoVpMHjhlsRabMXsyDuUTJOCIt1oA0ZmnQNk4j",
   "https://z68hkk8q7p.ufs.sh/f/upbel1v8YSdoCuSjObP6TLZ0sarCc5OWIqb4vXDzihoKu1wU",
@@ -10,12 +11,17 @@ const mockImages = mockUrls.map((url , index) => ({
   id:index+1,
   url: url,
 }));
-export default function HomePage() {
+export default async  function HomePage() {
+  const posts = await db.query.posts.findMany();
+  //console.log("Posts from DB:", posts);
   return (
     <main >
       <div className="flex flex-wrap gap-4">
-        {[...mockImages , ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages , ...mockImages].map((image , index) => (
+          <div key={image.id + "-"+index} className="w-48">
             <img src={image.url}/>
           </div>
         ))}
