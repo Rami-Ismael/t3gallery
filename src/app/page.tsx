9581399a07ae2a0e756b51c89d419ea986b835f2
Rami Ismael <rami.m.ismael@gmail.com>
2025-07-18 15:38:38 -0500

    Scaffold basic ui with mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
-
+import { id } from "zod/v4/locales";
+const mockUrls = [
+  "https://z68hkk8q7p.ufs.sh/f/upbel1v8YSdoVpMHjhlsRabMXsyDuUTJOCIt1oA0ZmnQNk4j",
+  "https://z68hkk8q7p.ufs.sh/f/upbel1v8YSdoCuSjObP6TLZ0sarCc5OWIqb4vXDzihoKu1wU",
+  "https://z68hkk8q7p.ufs.sh/f/upbel1v8YSdoWtbbkX4Mln3A5WCVzeOXaKvxogYI1Rf60iFr",
+  "https://z68hkk8q7p.ufs.sh/f/upbel1v8YSdoPdl8IbrprvDaTJZ3ShIknXU1ENG9s086Fo2M"
+]
+const mockImages = mockUrls.map((url , index) => ({
+  id:index+1,
+  url: url,
+}));
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello  ( gallery in posgress )
+    <main >
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages , ...mockImages].map((image) => (
+          <div key={image.id} className="w-48 p-2">
+            <img src={image.url}/>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
